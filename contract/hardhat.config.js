46fb8ce787ecd98c13a7d34ca930782548c291eb
coffiasd <759291502@qq.com>
2022-08-04 22:40:25 +0800

    add configs

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -3,4 +3,10 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    lukso: {
+      url: process.env.STAGING_LUKSO_URL,
+      accounts: [process.env.TEST_ACCOUNT_PRIVATE_KEY],
+    },
+  },
 };
